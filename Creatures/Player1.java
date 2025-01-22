fef8aba8f2ffcd9e70f2d72d25394ac2b03ad674
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2025-01-22 15:07:20 -0300

    0.04
    Implementando a melhorando as animações do personagem.

--- Creatures/Player1.java
+++ Creatures/Player1.java
@@ -29,71 +29,94 @@ public class Player1 extends Creatures{
         direction = "down";
     }
 
+    private int spriteIndex = 0;
+    private BufferedImage[] upSprites, downSprites, leftSprites, rightSprites;
+    
     public void getPlayerSprites(){
 
         try {
-
-            up1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_right_2.png"));
-
+          upSprites = new BufferedImage[]{
+            ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_up_1.png")),
+            ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_up_2.png"))
+          };
+          downSprites = new BufferedImage[]{
+            ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_down_1.png")),
+            ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_down_2.png"))
+          };
+          leftSprites = new BufferedImage[]{
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_left_1.png")),
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_left_2.png"))
+          };
+          rightSprites = new BufferedImage[]{
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_right_1.png")),
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Sprites/Character/man_right_2.png"))
+          };
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
     }
 
+
     public void update() {
+      Boolean moving = false;
       if (keyH.upPress) {
         direction = "up";
         y -= speed;
-    }
+        moving = true;
+      }
       if (keyH.downPress) {
         direction = "down";
         y += speed;
-    }
+        moving = true;
+      }
       if (keyH.leftPress) {
         direction = "left";
         x -= speed;
-    }
+        moving = true;
+      }
       if (keyH.rightPress) {
         direction = "right";
         x += speed;
+        moving = true;
       }
-  }
+
+      //lembrar de usar a lógica abaixo pra criar uma idle animation
+      if (moving) {
+        spriteCounter++;
+        if (spriteCounter > 10) { // Troca o sprite a cada 10 frames
+            spriteIndex = (spriteIndex + 1) % 2; // 2 é o número do sprite não esuqecer de alterar
+            spriteCounter = 0; //"%" serve para limitar quantas vezes spriteIndex vai rodar(no caso 2x)
+        }
+                 }
+}
 
     public void draw(Graphics2D g2){
 
         BufferedImage image = null;
         switch(direction){
+          
             case "up":
-             image = up1;
+             image = upSprites[spriteIndex];
              break;
+
              case "down":
-             image = down1;
+             image = downSprites[spriteIndex];
              break;
+
              case "left":
-             image = left1;
+             image = leftSprites[spriteIndex];
              break;
+
              case "right":
-             image = right1;
+             image = rightSprites[spriteIndex];
              break;
-        }
-        g2.drawImage(image, x, y,gp.tileSize, gp.tileSize, null);
+             }
+             
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+            /*
+        g2.setColor(Color.red); código altigo provavelmente inútil
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+                     //      width        height*/
+             }
 
-        //g2.setColor(Color.red);
-        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-                     //      width        height
-
-    }
-
-}
+ }
